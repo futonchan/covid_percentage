a9fae2de3a4006e6b6f844afc11f9ddd14c61d00
shouc <assdroi555@gmail.com>
2021-02-11 22:49:51 +0900

    tutorial post request

--- app/app.py
+++ app/app.py
@@ -1,20 +1,26 @@
-# アプリの動きを書くところらしい
+# アプリの動き(変数？)を書くところらしい
 
 #Flaskとrender_template（HTMLを表示させるための関数）をインポート
 from flask import Flask,render_template,request
 
-#Flaskインスタンスをappで宣言
-app = Flask(__name__)
+app = Flask(__name__) #Flaskインスタンスをappで宣言
 
 
-#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
-@app.route("/")
-@app.route("/index")
+# @...デコレータ、真下の行の関数を引数にとる
+# 呼び出し自体は真下の行の関数でおｋ、実行自体はデコレータが実行される
+@app.route("/") #「/」へアクセスがあった場合
+@app.route("/index") #「/index」へアクセスがあった場合
 def index():
     name = request.args.get("name")
     text = ["ABC","HIROKI","YANO"]
     return render_template("index.html",name=name,text=text) # index.htmlで使えるようになる
 
-#おまじない
+@app.route("/index", methods=["post"]) # index.htmlからPOSTを受け取る側
+def post():
+    name = request.form["name"] #
+    text = ["ABC","HIROKI","YANO"]
+    return render_template("index.html",name=name,text=text) # index.htmlで使えるようになる
+
+
 if __name__ == "__main__":
     app.run(debug=True)
