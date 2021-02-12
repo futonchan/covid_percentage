14d2885ce5c5ae20ff104bbffcfeacf5b6e583b7
shouc <assdroi555@gmail.com>
2021-02-12 11:29:13 +0900

    tutorial database input output

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@
 from flask import Flask,render_template,request
 
 from models.models import OnegaiContent
+from models.database import db_session # 普通の変数もインポートできるんだ
+from datetime import datetime
 
 app = Flask(__name__) #Flaskインスタンスをappで宣言
 
@@ -21,10 +23,18 @@ def index():
 
 @app.route("/index", methods=["post"]) # index.htmlからPOSTを受け取る側
 def post():
-    name = request.form["name"] #postした後はdef index()のページなくなる
+    name = request.form["name"] # index.htmlのフォームのname指定した変数をnameに代入
     all_onegai = OnegaiContent.query.all()
     return render_template("index.html",name=name,all_onegai=all_onegai) # index.htmlで使えるようになる
 
+@app.route("/add", methods=["post"])
+def add():
+    title = request.form["title"]
+    body = request.form["body"]
+    content = OnegaiContent(title,body,datetime.now())
+    db_session.add(content)
+    db_session.commit()
+    return index() # returnに関数渡すこともできる
 
 if __name__ == "__main__":
     app.run(debug=True)
