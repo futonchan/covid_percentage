e5056b5f048144da64cd32fae72ae76b3a77334e
shouc <assdroi555@gmail.com>
2021-02-26 12:13:15 +0900

    irarani file seiri

--- app/app.py
+++ app/app.py
@@ -17,21 +17,7 @@ Bootstrap = Bootstrap(app)
 # @...デコレータ、真下の行の関数を引数にとる
 # 呼び出し自体は真下の行の関数でおｋ、実行自体はデコレータが実行される
 @app.route("/") #「/」へアクセスしたとき
-@app.route("/index") #「/index」にアクセスしたとき
 def index():
-    print("def index")
-    name = request.args.get("name")
-    all_onegai = OnegaiContent.query.all() # SELECT ** FROM XX
-
-    return render_template("index.html",name=name,all_onegai=all_onegai) # index.htmlで使えるようになる
-
-@app.route("/index", methods=["post"]) #htmlのformactionの名前が/indexで、POSTを受け取ったとき
-def post():
-    print("def post index")
-    # name = request.form["name"] # index.htmlのフォームのname指定した変数をnameに代入
-    # all_onegai = OnegaiContent.query.all() # SELECT ** FROM XX
-    # [<Title 'fdsaf'>, <Title 'っっっっっっｆ'>, <Title 'aaa'>] DBをリスト構造で全て出力された結果
-
     # 人口取得 now_covids=now_covids
     path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/csv/nhk_news_covid19_prefectures_daily_data.csv")
     print(path)
@@ -49,37 +35,7 @@ def post():
 
 
     # ここで現在の感染者数と、人口を渡す
-    return render_template("jmap_test.html", pref_covid_total_dict=prefnames) # index.htmlで使えるようになる
-
-@app.route("/add", methods=["post"]) # htmlのformactionの名前が/addからpostを受け取った時
-def add():
-    print("def add")
-    title = request.form["title"]
-    body = request.form["body"]
-    content = OnegaiContent(title,body,datetime.now())
-    db_session.add(content) # db_session... データベース接続用のインスタンス、database.pyで宣言してる
-    db_session.commit()
-    return index() # returnに関数渡すこともできる
-
-@app.route("/update",methods=["post"]) # htmlのformactionの名前がupdate
-def update():
-    print("def update")
-    print(request.form) # ImmutableMultiDict([('title', 'fff'), ('body', 'fffdsa'), ('update', '5')])
-    content = OnegaiContent.query.filter_by(id=request.form["update"]).first()
-    content.title = request.form["title"]
-    content.body = request.form["body"]
-    db_session.commit()
-    return index() # この後にindex()も実行するので、db表示される
-
-@app.route("/delete",methods=["post"])
-def delete():
-    print("def delete")
-    id_list = request.form.getlist("delete")
-    for id in id_list:
-        content = OnegaiContent.query.filter_by(id=id).first()
-        db_session.delete(content)
-    db_session.commit()
-    return index()
+    return render_template("index.html", pref_covid_total_dict=prefnames) # index.htmlで使えるようになる
 
 if __name__ == "__main__":
     app.run(debug=True)
